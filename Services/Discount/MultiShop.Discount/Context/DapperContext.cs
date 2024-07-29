08f9b2969f4941433f7f3f8d555abb4df5af6676
ihsantapan <tapanihsan@gmail.com>
2024-07-29 15:53:29 +0300

    Discount domain için Entity,Context,Dtos oluşturuldu

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DapperContext:DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server=LAPTOP-7FU59OPV;Database=myDataBase;Uid=sa;Pwd=10081991;");
+            optionsBuilder.UseSqlServer("Server=LAPTOP-7FU59OPV; initial Catalog=MultiShopDiscountDb; integrated Security=true");
+        }
+
+        public DbSet<Coupon> Coupons{ get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+    }
+}
