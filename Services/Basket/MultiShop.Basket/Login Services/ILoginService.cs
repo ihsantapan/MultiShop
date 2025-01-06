3ad1d1837ff3b4771544955dc6351bf6a0ca5b36
ihsan tapan <tapanihsan@gmail.com>
2025-01-06 23:14:02 +0300

    Basket controller oluşturuldu

---
+++ Services/Basket/MultiShop.Basket/Login Services/ILoginService.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShop.Basket.Login_Services
+{
+    public interface ILoginService
+    {
+        public string GetUserId { get; }
+    }
+}
---
+++ Services/Basket/MultiShop.Basket/Login Services/LoginService.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Basket.Login_Services
+{
+    public class LoginService : ILoginService
+    {
+        public readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LoginService(IHttpContextAccessor contextAccessor)
+        {
+            _httpContextAccessor = contextAccessor;
+        }
+
+        public string GetUserId => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
