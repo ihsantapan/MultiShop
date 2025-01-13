f8332e27630f0be532e34d16a08ccd1496dc881c
ihsan tapan <tapanihsan@gmail.com>
2025-01-13 23:27:42 +0300

    Admin UI hazırlanıyor.

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
+            ViewBag.v0 = "Kategori İşlemleri";
+            return View();
+        }
+    }
+}
