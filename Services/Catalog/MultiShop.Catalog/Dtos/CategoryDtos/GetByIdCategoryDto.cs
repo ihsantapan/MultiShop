e36ac2938b587fae33efac52caef31d4be2b2d85
ihsan tapan <tapanihsan@gmail.com>
2025-01-20 04:04:21 +0300

    Öne çıkan kategoriler backend frontend bağlantısı sağlandı. Verilerin dinamik gelmesi sağlandı.

--- Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/GetByIdCategoryDto.cs
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/GetByIdCategoryDto.cs
@@ -3,7 +3,8 @@
     public class GetByIdCategoryDto
     {
         public string CategoryId { get; set; }
-
         public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+
     }
 }
--- Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/UpdateCategoryDto.cs
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/UpdateCategoryDto.cs
@@ -3,7 +3,7 @@
     public class UpdateCategoryDto
     {
         public string CategoryId { get; set; }
-
+        public string ImageUrl { get; set; }
         public string CategoryName { get; set; }
 
     }
