372043a8005f4975254456b70f7da7b7fb0086a7
ihsan tapan <tapanihsan@gmail.com>
2025-01-21 22:13:16 +0300

    Brand backend frontend bağlantısı sağlandı. Verilerin dinamik gelmesi sağlandı.

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/BrandDtos/UpdateBrandDto.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Dtos.BrandDtos
+{
+    public class UpdateBrandDto
+    {
+        public string BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
 using MultiShop.Catalog.Mapping;
+using MultiShop.Catalog.Services.BrandServices;
 using MultiShop.Catalog.Services.CategoryServices;
 using MultiShop.Catalog.Services.FeatureServices;
 using MultiShop.Catalog.Services.FeatureSliderService;
@@ -33,6 +34,7 @@ builder.Services.AddScoped<IFeatureSliderService, FeatureSliderService>();
 builder.Services.AddScoped<ISpecialOfferService, SpecialOfferService>();
 builder.Services.AddScoped<IFeatureService, FeatureService>();
 builder.Services.AddScoped<IOfferDiscountService, OfferDiscountService>();
+builder.Services.AddScoped<IBrandService, BrandService>();
 
 
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
